e30ed358bc028cfaaa5b739677f7fc980f446188
pshenmic <pshenmic@gmail.com>
2025-03-10 03:10:13 +0700

    Add links to PE

--- app/components/TorrentListItem.jsx
+++ app/components/TorrentListItem.jsx
@@ -2,7 +2,7 @@ export default function TorrentListItem ({torrent}) {
   return (
     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
       <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-        {torrent.identifier}
+        <a href={`https://testnet.platform-explorer.com/document/${torrent.identifier}`}>{torrent.identifier}</a>
       </th>
       <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
         {torrent.name}
@@ -14,11 +14,11 @@ export default function TorrentListItem ({torrent}) {
         <a className={"hover:bg-sky-700"} href={torrent.magnet}>{torrent.magnet}</a>
       </td>
       <td className="px-6 py-4">
-        {torrent.owner.identifier}
+        <a href={`https://testnet.platform-explorer.com/identity/${torrent.owner.identifier}`}>{torrent.owner.identifier}</a>
       </td>
       <td className="px-6 py-4">
         {new Date(torrent.timestamp).toLocaleString()}
       </td>
     </tr>
-)
+  )
 }
