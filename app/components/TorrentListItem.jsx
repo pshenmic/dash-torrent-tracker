026580dc52122bfacf63aee554ca1bb940e8b0f0
pshenmic <pshenmic@gmail.com>
2025-03-10 07:25:59 +0700

    Open links in new tab

--- app/components/TorrentListItem.jsx
+++ app/components/TorrentListItem.jsx
@@ -2,7 +2,7 @@ export default function TorrentListItem ({torrent}) {
   return (
     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
       <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-        <a href={`https://testnet.platform-explorer.com/document/${torrent.identifier}`}>{torrent.identifier}</a>
+        <a target={'_blank'} href={`https://testnet.platform-explorer.com/document/${torrent.identifier}`}>{torrent.identifier}</a>
       </th>
       <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
         {torrent.name}
@@ -14,7 +14,7 @@ export default function TorrentListItem ({torrent}) {
         <a className={"hover:bg-sky-700"} href={torrent.magnet}>{torrent.magnet}</a>
       </td>
       <td className="px-6 py-4">
-        <a href={`https://testnet.platform-explorer.com/identity/${torrent.owner.identifier}`}>{torrent.owner.identifier}</a>
+        <a target={'_blank'} href={`https://testnet.platform-explorer.com/identity/${torrent.owner.identifier}`}>{torrent.owner.identifier}</a>
       </td>
       <td className="px-6 py-4">
         {new Date(torrent.timestamp).toLocaleString()}
