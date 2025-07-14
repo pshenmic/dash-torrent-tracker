63782d28a21f44e1b42db61ca64bf579f8feeeba
pshenmic <pshenmic@gmail.com>
2025-07-15 02:54:45 +0700

    Temporarily use bundled sdk

--- app/hooks/useSdk.js
+++ app/hooks/useSdk.js
@@ -1,12 +1,12 @@
-import {DashPlatformSDK} from 'dash-platform-sdk'
+import {DashPlatformSDK} from 'dash-platform-sdk/bundle.min.js'
 
 let dashPlatformSDK
 
 export const useSdk = () => {
 
-  // if (window.dashPlatformSDK) {
-  //   return window.dashPlatformSDK
-  // }
+  if (window.dashPlatformSDK) {
+    return window.dashPlatformSDK
+  }
 
   if (!dashPlatformSDK) {
     dashPlatformSDK = new DashPlatformSDK()
